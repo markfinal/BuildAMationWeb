3041d76bc1bdfb0e998e8493221aa7661283546f
Mark Final <markfinal@hotmail.com>
2014-12-02 21:03:07 +0000

    Dynamically create a TOC from the contents

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,36 @@
+function rebuildTOC() {
+	var ToC =
+		"<div class='table-of-contents'>" +
+		"<h2>On this page:</h2>" +
+		"<ul>";
+	var newLine, el, title, link;
+	$("#content h1").each(function() {
+		el = $(this);
+		title = el.text();
+		link = "#" + el.attr("id");
+		newLine =
+			"<li>" +
+			"<a href='" + link + "'>" +
+			title +
+			"</a>" +
+			"</li>";
+		ToC += newLine;
+	});
+	ToC +=
+		"</ul>" +
+		"</div>";
+	return ToC;
+}
+
 $(function() {
 	$('#home').click(function() {
 		$('#content').load('content/introduction.html');
 	});
 	$('#install').click(function() {
-		$('#content').load('content/installmono.html');
+		$('#content').load('content/installmono.html', function() {
+			ToC = rebuildTOC();
+			$('#content').prepend(ToC);
+		});
 	});
 	$('#contact').click(function() {
 		window.alert("Contact details not yet available");
