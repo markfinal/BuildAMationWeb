31ef2b3b2cb5158c9f61bf851cd47147e994b104
Mark Final <markfinal@hotmail.com>
2014-12-02 13:56:37 +0000

    Mark some features as unsupported currently

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,4 +5,10 @@ $(function() {
 	$('#install').click(function() {
 		$('#content').load('content/installmono.html');
 	});
+	$('#contact').click(function() {
+		window.alert("Contact details not yet available");
+	});
+	$('#license').click(function() {
+		window.alert("License details not yet available");
+	});
 });
