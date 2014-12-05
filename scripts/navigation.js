b7eb00fd54c3372c13e34cfe98b4c9ff34c6598d
Mark Final <markfinal@hotmail.com>
2014-12-05 18:45:14 +0000

    Removing old toc generation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,27 +1,3 @@
-function rebuildToC() {
-	var ToC =
-		"<div class='table-of-contents'>" +
-		"<h2>On this page:</h2>" +
-		"<ul>";
-	var newLine, el, title, link;
-	$("#content h1").each(function() {
-		el = $(this);
-		title = el.text();
-		link = "#" + el.attr("id");
-		newLine =
-			"<li>" +
-			"<a href='" + link + "'>" +
-			title +
-			"</a>" +
-			"</li>";
-		ToC += newLine;
-	});
-	ToC +=
-		"</ul>" +
-		"</div>";
-	return ToC;
-}
-
 function updateContentWithToC() {
 	$('#toc').toc(
 	{
